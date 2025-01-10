ad68bc125c6af51e4d23463598dc6fc24fda927d
Kaisar Attar <117891197+kaisar1232@users.noreply.github.com>
2025-01-10 16:39:00 +0530

    Add files via upload

---
+++ program404.java
@@ -0,0 +1,19 @@
+class node
+{
+    public int data;
+    public node next;
+}
+
+class SinglyLL
+{
+    public node First;
+    public int iCount; 
+}
+
+class program404
+{
+    public static void main(String Arg[])
+    {
+        SinglyLL obj = new SinglyLL();
+    }
+}
